462bd72af78f52eb55a5a0c04bd7e54f759a6d10
Mubashir <zainarshad45122@gmail.com>
2019-11-26 18:33:45 +0500

    First Commit

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("E:\\Andromeda Technologies\\My Andromeda Projects\\Matt\\gatsby-project\\.cache\\dev-404-page.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("E:\\Andromeda Technologies\\My Andromeda Projects\\Matt\\gatsby-project\\src\\pages\\index.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("E:\\Andromeda Technologies\\My Andromeda Projects\\Matt\\New folder\\ayenem\\.cache\\dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("E:\\Andromeda Technologies\\My Andromeda Projects\\Matt\\New folder\\ayenem\\src\\pages\\index.js")))
 }
 
